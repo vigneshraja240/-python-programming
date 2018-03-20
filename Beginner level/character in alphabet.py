3bd9ad3e85bbfb320a3bc002ca2dac87ebef151e
vigneshraja240 <signin780@gmail.com>
2018-03-19 22:42:59 -0700

    Update character in alphabet.py

--- Beginner level/character in alphabet.py
+++ Beginner level/character in alphabet.py
@@ -2,7 +2,7 @@ x=raw_input()
 if x.isdigit():
 	print "No"
 else:
-	if (x>'a' and x<'z') and (x>'A' and x<'z'):
+	if (x>='a' and x<='z') or (x>='A' and x<='Z'):
 		print "character is in alphabet"
 	else:
 		print "No"
