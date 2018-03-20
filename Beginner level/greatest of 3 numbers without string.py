1d791d69320ca605938ec6c906c8a91b9bb97486
vigneshraja240 <signin780@gmail.com>
2018-03-20 06:39:35 -0700

    Update greatest of 3 numbers without string.py

--- Beginner level/greatest of 3 numbers without string.py
+++ Beginner level/greatest of 3 numbers without string.py
@@ -1,11 +1,16 @@
 a=int(raw_input())
 b=int(raw_input())
 c=int(raw_input())
-if(a>b and a>c):
-	print a
-elif(b>a and b>c):
-	print b
-elif(c>a and c>b):
-	print c
-else:
-	print"invalid"
+if isinstance(a,str) and isinstance(b,str) and isinstance(c,str):
+	if(((a>='a') and (a<='z')) and ((b>='a') and (b<='z')) and ((c>='a') and (c<='z'))):
+		print"Invalid Input"
+	else:
+		a=int(a)
+		b=int(b)
+		c=int(c)
+		if(a>b and a>c):
+			print a
+		elif(b>a and b>c):
+			print b
+		else:
+			print c
