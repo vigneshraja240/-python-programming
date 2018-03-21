460b907c549defd2101841f8444f511a28bba0c8
vigneshraja240 <signin780@gmail.com>
2018-03-20 21:55:22 -0700

    Update leap year.py

--- Beginner level/leap year.py
+++ Beginner level/leap year.py
@@ -1,9 +1,9 @@
 a=raw_input()
 if a.isdigit():
-	if(int(a)%4==0):
+	if((int(a)%4==0) and ((int(a)%100!=0) or (int(a)%400==0))):
 		print"yes"
 	else:
-		print"no"
+		print"no"	
 else:
 	print"Invalid Input"
 
